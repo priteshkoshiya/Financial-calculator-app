15574c9a28a2f4da5b67d49190c8617b069c6ede
Priteshkoshiya <priteshgkoshiya@gmail.com>
2026-03-24 18:17:52 +0530

    chore: remove github pages config for vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/Financial-calculator-app",
-  assetPrefix: "/Financial-calculator-app",
   typescript: {
     ignoreBuildErrors: true,
   },
