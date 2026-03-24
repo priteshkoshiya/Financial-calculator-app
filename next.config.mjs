41339d255f0f39b28e59d82ac6ebc8e00556c443
Priteshkoshiya <priteshgkoshiya@gmail.com>
2026-03-24 18:12:58 +0530

    fix: set basePath for github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/Financial-calculator-app",
+  assetPrefix: "/Financial-calculator-app",
   typescript: {
     ignoreBuildErrors: true,
   },
